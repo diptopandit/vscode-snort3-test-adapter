71f81fa36fa1602cb2d6a8fe32a8849df78d53b9
Dipto Pandit (dipandit) <dipandit@cisco.com>
2021-01-14 16:14:29 +0530

    inital commit

--- src/adapter.ts
+++ src/adapter.ts
@@ -1,19 +1,51 @@
 import * as vscode from 'vscode';
-import { TestAdapter, TestLoadStartedEvent, TestLoadFinishedEvent, TestRunStartedEvent, TestRunFinishedEvent, TestSuiteEvent, TestEvent } from 'vscode-test-adapter-api';
+import { TestSuiteInfo, TestInfo, TestAdapter, TestLoadStartedEvent, TestLoadFinishedEvent, TestRunStartedEvent, TestRunFinishedEvent, TestSuiteEvent, TestEvent } from 'vscode-test-adapter-api';
 import { Log } from 'vscode-test-adapter-util';
-import { loadFakeTests, runFakeTests } from './fakeTests';
+import { loadSnort3Tests, snort3Test, runTest } from './snort3Test';
 
-/**
- * This class is intended as a starting point for implementing a "real" TestAdapter.
- * The file `README.md` contains further instructions.
- */
+class jobQueue {
+	private jobdata = new Array<TestInfo|TestSuiteInfo>();
+
+	public next():TestInfo|undefined{
+		const nextElement = this.jobdata.shift();
+		if(nextElement)
+		{
+			if(nextElement.type==='test')
+				return nextElement;
+			else if(nextElement.type==='suite')
+			{
+				var tmp = nextElement.children.pop();
+				while(tmp !== undefined)
+				{
+					this.jobdata.unshift(tmp);
+					tmp = nextElement.children.pop();
+				}
+				return this.next();
+			}
+		}
+		return;
+	}
+
+	public post(newJob:TestInfo|TestSuiteInfo):number{
+		return this.jobdata.push(newJob);
+	}
+
+	public flush(){
+		this.jobdata.splice(0);
+	}
+}
 export class ExampleAdapter implements TestAdapter {
 
 	private disposables: { dispose(): void }[] = [];
+	public loadedTests: {suite:TestSuiteInfo, snort3Tests:Map<string,snort3Test>}=<{suite:TestSuiteInfo, snort3Tests:Map<string,snort3Test>}>{};
+	private currentJobQ:jobQueue;
+	private running:boolean = false;
+	//private active_jobs:snort3Test[]=[];
 
 	private readonly testsEmitter = new vscode.EventEmitter<TestLoadStartedEvent | TestLoadFinishedEvent>();
 	private readonly testStatesEmitter = new vscode.EventEmitter<TestRunStartedEvent | TestRunFinishedEvent | TestSuiteEvent | TestEvent>();
 	private readonly autorunEmitter = new vscode.EventEmitter<void>();
+	private concurrency:number = 3;
 
 	get tests(): vscode.Event<TestLoadStartedEvent | TestLoadFinishedEvent> { return this.testsEmitter.event; }
 	get testStates(): vscode.Event<TestRunStartedEvent | TestRunFinishedEvent | TestSuiteEvent | TestEvent> { return this.testStatesEmitter.event; }
@@ -24,37 +56,61 @@ export class ExampleAdapter implements TestAdapter {
 		private readonly log: Log
 	) {
 
-		this.log.info('Initializing example adapter');
+		this.log.info('Initializing snort3_test adapter');
 
 		this.disposables.push(this.testsEmitter);
 		this.disposables.push(this.testStatesEmitter);
 		this.disposables.push(this.autorunEmitter);
+		this.currentJobQ = new jobQueue;
 
+	}
+
+	private findNode(searchNode: TestSuiteInfo | TestInfo, id:string):TestSuiteInfo | TestInfo | undefined {
+		if (searchNode.id === id) {
+			return searchNode;
+		} else if (searchNode.type === 'suite') {
+			for (const child of searchNode.children) {
+				const found = this.findNode(child, id);
+				if (found) return found;
+			}
+		}
+		return undefined;
 	}
 
 	async load(): Promise<void> {
-
-		this.log.info('Loading example tests');
-
+		this.log.info('Loading snort3 tests');
 		this.testsEmitter.fire(<TestLoadStartedEvent>{ type: 'started' });
-
-		const loadedTests = await loadFakeTests();
-
-		this.testsEmitter.fire(<TestLoadFinishedEvent>{ type: 'finished', suite: loadedTests });
-
+		this.loadedTests= await loadSnort3Tests(this.workspace);
+		this.testsEmitter.fire(<TestLoadFinishedEvent>{ type: 'finished', suite: this.loadedTests.suite });
 	}
 
 	async run(tests: string[]): Promise<void> {
-
-		this.log.info(`Running example tests ${JSON.stringify(tests)}`);
-
+		this.log.info(`Running snort3 tests ${JSON.stringify(tests)}`);
 		this.testStatesEmitter.fire(<TestRunStartedEvent>{ type: 'started', tests });
+		for (const suiteOrTestId of tests) {
+			const node = this.findNode(this.loadedTests.suite, suiteOrTestId);
+			if (node) this.currentJobQ.post(node);
+		}
+		if(this.running) return Promise.resolve();
+		this.running = true;
+		var PromisePool = require('es6-promise-pool')
+		const self = this;
+		var promiseProducer = function () {
+			const node = self.currentJobQ.next();
+			if(node)
+			{
+				const test = self.loadedTests.snort3Tests.get(node.id);
+				return runTest(node.id,test,self.testStatesEmitter);
+			}
+			else return null;
+		}
+		  
+		var pool = new PromisePool(promiseProducer, this.concurrency)
+		  
+		await pool.start()
 
-		// in a "real" TestAdapter this would start a test run in a child process
-		await runFakeTests(tests, this.testStatesEmitter);
-
-		this.testStatesEmitter.fire(<TestRunFinishedEvent>{ type: 'finished' });
-
+		this.running=false;
+		this.testStatesEmitter.fire(<TestRunFinishedEvent>{ type: 'finished' });	
 	}
 
 /*	implement this method if your TestAdapter supports debugging tests
@@ -64,8 +120,7 @@ export class ExampleAdapter implements TestAdapter {
 */
 
 	cancel(): void {
-		// in a "real" TestAdapter this would kill the child process for the current test run (if there is any)
-		throw new Error("Method not implemented.");
+		this.currentJobQ.flush();
 	}
 
 	dispose(): void {
---
+++ src/snort3Test.ts
@@ -0,0 +1,94 @@
+import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as convert from 'xml-js';
+import * as child_process from 'child_process';
+import { TestSuiteInfo, TestInfo, TestRunStartedEvent, TestRunFinishedEvent, TestSuiteEvent, TestEvent } from 'vscode-test-adapter-api';
+
+export class snort3Test {
+	private testData:any;
+	private active_child:child_process.ChildProcess|undefined;
+	constructor(xmlPath:string){
+		const xmlFile = fs.readFileSync(xmlPath, 'utf8');
+		this.testData = JSON.parse(convert.xml2json(xmlFile, {compact: true, spaces: 2})); 
+	}
+
+	getName():string {return this.testData["snort-test"].name._text;}
+	getDescription():string {return this.testData["snort-test"].description._text;}
+	execute(id:string, testStatesEmitter:vscode.EventEmitter<TestRunStartedEvent |
+		TestRunFinishedEvent | TestSuiteEvent | TestEvent>):Promise<void>
+	{
+		return new Promise((resolve)=>{
+			const num = (Math.floor(Math.random() * 9)+1).toString();
+			this.active_child=child_process.spawn('sleep',[num]).once('exit', () => {
+				testStatesEmitter.fire(<TestEvent>{ type: 'test', test: id, state: 'passed' });
+				resolve();
+			});
+		});
+	}
+
+	abort()
+	{
+		if(undefined !== this.active_child) this.active_child.kill();
+	}
+}
+
+export function loadSnort3Tests(rootdir:vscode.WorkspaceFolder)
+	:Promise<{suite:TestSuiteInfo,snort3Tests:Map<string,snort3Test>}>
+{
+	var sampleTestSuit: TestSuiteInfo = {
+		type: 'suite',
+		id: 'Snort3_test_root',
+		label: 'Snort3_test_root',
+		children: []
+	};
+	var snort3Tests = new Map<string,snort3Test>();
+	const getLastItem = (thePath: string) => thePath.substring(thePath.lastIndexOf('/') + 1)
+	var walk = function(dir:string) {
+		var list = fs.readdirSync(dir);
+		if(list.includes('test.xml')){
+			//leaf
+			const file:string = dir + '/test.xml';
+			const thisTest = new snort3Test(file);
+			snort3Tests.set(dir,thisTest);
+			return <TestInfo>{type: 'test',
+				id: dir,
+				label: getLastItem(dir),
+				file: file,
+				description: thisTest.getName(),
+				tooltip:thisTest.getName() + thisTest.getDescription()
+			};
+		} else {
+			var result:TestSuiteInfo={
+				type:'suite',
+				id:dir,
+				label:getLastItem(dir),
+				children:[]
+			};
+			list.forEach(function(file) {
+				file = dir + '/' + file;
+				var stat = fs.statSync(file);
+				if (stat && stat.isDirectory()) {
+					var node = walk(file);
+					if(node !== undefined)
+						result.children.push(node);
+				}
+			});
+			if(result.children.length>0)
+				return result;
+			return undefined;
+		}
+	}
+	var suite=walk(rootdir.uri.path);
+	if(suite !== undefined)
+		sampleTestSuit.children.push(suite);
+	return Promise.resolve({suite:sampleTestSuit,snort3Tests:snort3Tests});	
+}
+
+export function runTest(id:string, test:snort3Test|undefined,
+	testStatesEmitter:vscode.EventEmitter<TestRunStartedEvent |
+	TestRunFinishedEvent | TestSuiteEvent | TestEvent>):Promise<void>
+{
+	testStatesEmitter.fire(<TestEvent>{ type: 'test', test: id, state: 'running' });
+	if(test) return test.execute(id,testStatesEmitter);
+	else return Promise.resolve();
+}
